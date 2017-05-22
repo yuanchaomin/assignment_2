9759346c2d53e615b7fe7b971419f51c7d96cf15
Chaomin <lycanfirst@gmail.com>
2017-05-22 23:01:11 +1000

    sort_list_test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,8 +1,39 @@
+import org.apache.commons.collections.map.MultiValueMap;
+
+import java.util.*;
+
 /**
  * Created by Chaomin on 5/19/2017.
  */
 public class test {
-    String a = "asdasd";
+    public static void main(String[] args) {
+        MultiValueMap dict = new MultiValueMap();
+        dict.put(1,10);
+        dict.put(1,9);
+        dict.put(1,5);
+        dict.put(2,6);
+        dict.put(2,7);
+        dict.put(3,9);
+        dict.put(3,10);
+
+        List<MultiValueMap.Entry<Integer, Integer >> list =
+                new LinkedList<MultiValueMap.Entry<Integer, Integer >>(dict.entrySet());
+                Collections.sort(list, new Comparator<HashMap.Entry<Integer, Integer>>() {
+                    @Override
+                    public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                        return (o1.getValue()).compareTo(o2.getValue());
+                    }
+                });
+
+
+        Map<Integer,Integer> result = new MultiValueMap();
+        for (Map.Entry<Integer,Integer> entry : list){
+            result.put( entry.getKey(), entry.getValue());
+        }
+
+        System.out.println(result);
+    }
+
 
 
 }
